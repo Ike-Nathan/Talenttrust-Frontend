0e3f70a6ad29f3d07cfe3371ca86d60371f6343c
Aman koli <2025.amana@isu.ac.in>
2026-06-24 19:58:07 +0530

    fix: resolve failing build and tests on main branch

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,4 @@
-const js = require('@eslint/js');
-const tseslint = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
+const nextConfig = require('eslint-config-next');
 
 module.exports = [
   {
